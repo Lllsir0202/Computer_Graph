4f8683f3ed1c4ff0eb08b7ade7d975c07884a75e
Celesteria <2210878@mail.nankai.edu.cn>
2024-12-25 01:21:40 +0800

    Fix dielectric

--- code/components/simple_path_tracing/src/shaders/Dielectric.cpp
+++ code/components/simple_path_tracing/src/shaders/Dielectric.cpp
@@ -43,18 +43,16 @@ namespace SimplePathTracer
         float NdotV       = glm::abs(glm::dot(n, -ray.direction));
         float G           = GeometrySchlickGGX(NdotV, 0.5f);
         Vec3  attenuation = f * D * G / (4.f * glm::abs(glm::dot(Wr, n)) * glm::abs(glm::dot(ray.direction, n)));
-        Vec3  Wi          = -ray.direction;
 
-        float ni_nt    = glm::dot(normal, Wi) > 0 ? (1.0f / ior) : (ior / 1.0f);
-        float cos_val  = abs(glm::dot(n, Wi));
+        float ni_nt    = glm::dot(normal, -ray.direction) > 0 ? (1.0f / ior) : (ior / 1.0f);
+        float cos_val  = abs(glm::dot(n, -ray.direction));
         float sin_val  = max(0.f, 1.f - cos_val * cos_val);
         float sin_val2 = ni_nt * ni_nt * sin_val;
         if (sin_val2 >= 1.0f - 0.00001f) return {Ray{hitPoint, Wr}, attenuation, Vec3{0}, 1.0f};
 
-        float cos_T        = sqrt(1 - sin_val2);
-        Vec3  Wt           = ni_nt * (-Wi) + (ni_nt * cos_val - cos_T) * n;
-        Wt                 = glm::normalize(Wt);
-        f                  = absorbed + (Vec3(1.0, 1.0, 1.0) - absorbed) * pow(1.0f - abs(glm::dot(n, Wi)), 5.0f);
+        float cos_T = sqrt(1 - sin_val2);
+        Vec3  Wt    = glm::normalize(ni_nt * ray.direction + (ni_nt * cos_val - cos_T) * n);
+        f           = absorbed + (Vec3(1.0, 1.0, 1.0) - absorbed) * pow(1.0f - abs(glm::dot(n, -ray.direction)), 5.0f);
         Vec3 r_attenuation = (1.f / (ni_nt * ni_nt)) * (Vec3{1.0, 1.0, 1.0} - f) / abs(cos_val);
 
         return {Ray{hitPoint, Wr}, attenuation, Vec3{0}, 1.0f, true, Ray{hitPoint, Wt}, r_attenuation, 1.0f};
